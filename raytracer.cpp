bfeb38af4c5fc52fe22cef82ff60d1e9d15a9066
MaddinK <martin.koeppel@gmx.net>
2016-12-17 19:07:16 +0100

    improved function intersection

--- raytracer.cpp
+++ raytracer.cpp
@@ -48,22 +48,21 @@ struct Sphere {
 
     // get intersection with ray: refer: https://en.wikipedia.org/wiki/Line%E2%80%93sphere_intersection
     // more details: https://www.scratchapixel.com/lessons/3d-basic-rendering/minimal-ray-tracer-rendering-simple-shapes/ray-sphere-intersection
-    bool intersection(const Vec &centerPointSphere, const double &radiusSphere, const Ray &RayIntersect,
-                      double &distance)
+    bool intersection(const Ray &RayIntersect, double &distance)
     {
         // (l * (o - c))^2 - || o - c ||^2 + r^2
         double val1, val2, val3, dist1, dist2;
-        val1 = (RayIntersect.origin-centerPointSphere).dotProduct(RayIntersect.direction);
-        val2 = (RayIntersect.origin-centerPointSphere).dotProduct((RayIntersect.origin-centerPointSphere));
-        val3 = val1 - val2 + pow(radiusSphere, 2.0);
+        val1 = (RayIntersect.origin-centerPoint).dotProduct(RayIntersect.direction);
+        val2 = (RayIntersect.origin-centerPoint).dotProduct((RayIntersect.origin-centerPoint));
+        val3 = val1 - val2 + pow(radius, 2.0);
 
         if(val3 < 0) {
             return false;
         }
 
         // compute distance
-        dist1 = -(RayIntersect.origin-centerPointSphere).dotProduct(RayIntersect.direction) + sqrt(val3);
-        dist2 = -(RayIntersect.origin-centerPointSphere).dotProduct(RayIntersect.direction) - sqrt(val3);
+        dist1 = -(RayIntersect.origin-centerPoint).dotProduct(RayIntersect.direction) + sqrt(val3);
+        dist2 = -(RayIntersect.origin-centerPoint).dotProduct(RayIntersect.direction) - sqrt(val3);
 
         if(dist1 < 0 && dist2 > 0){
             distance = dist2;
