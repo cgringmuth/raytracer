4ba3a1cc892be74e237d110872839ecf62bcd4c5
Christoph Hoffmann <choffmann1983@gmail.com>
2016-12-31 17:15:40 +0100

    added some results

--- update-run.sh
+++ update-run.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-outFilename="out14"
+outFilename="out17"
 (cd build; make;) && time ./build/raytracer -o results/$outFilename.png | tee raytracer.log
 # convert $outFilename.ppm $outFilename.png
