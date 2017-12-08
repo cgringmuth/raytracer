da3be08f23a9d8e10dec71d4184fda683f4039b4
Christoph Hoffmann <choffmann1983@gmail.com>
2017-12-08 17:48:48 +0100

    added first scene yaml

--- src/scene.cpp
+++ src/scene.cpp
@@ -1,14 +1,22 @@
 //
 // Created by chris on 10.10.17.
 //
-
+#include "opencv2/opencv.hpp"
 #include "scene.h"
+#include <string>
 
-Scene::Scene() {
+Scene::Scene(const std::string& filename) {
     // TODO: loading from YAML file
+    cv::FileStorage fs(filename, cv::FileStorage::READ);
 
+
+    fs.release();
 }
 
-const std::vector<std::shared_ptr<Primitive>> &Scene::getObjects() const {
+const std::vector<std::unique_ptr<Primitive>> &Scene::getObjects() const {
     return objects;
+}
+
+const std::vector<Light> &Scene::getLights() const {
+    return lights;
 }
--- update-run.sh
+++ update-run.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-outFilename="out35"
-(cd build; make;) && time ./build/raytracer -o results/$outFilename.png | tee raytracer.log
+outFilename="out1"
+(cd build; make;) && time ./build/raytracer scene.yaml -o results/$outFilename.png | tee raytracer.log
 # convert $outFilename.ppm $outFilename.png
